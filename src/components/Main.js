3c0904f6f66334e1324b3420ddc1bffaad5cbf61
kiboma <omayioben@gmail.com>
2023-12-06 18:56:11 +0300

    Render vehicles on webpage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Main = () => {
     const main_style={
         minHeight : '70vh',
     }
+
+    const [url, setUrl]=useState("http://localhost:8000/vehicles/")
+
   return (
-    <div style={main_style}>
+    <section style={main_style}>
+        {url}
+        {url.map(vehicle=> (
+            <div>
+                <span>{vehicle.name}</span>
+            </div>
+        )) }
+
+
 
       
-    </div>
+    </section>
   )
 }
 
