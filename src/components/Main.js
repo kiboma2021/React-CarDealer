c05ee69975f23a0ef6021d9f49cc33d1477ad5d6
kiboma <omayioben@gmail.com>
2023-12-07 08:37:08 +0300

    fetch vehicles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import carImage from '../assets/car.webp'
 
 const Main = () => {
     const main_style={
         minHeight : '70vh',
     }
 
-    const [url, setUrl]=useState("http://localhost:8000/vehicles/")
+    const [vehicles, setVehicles]=useState([]);
+
+    const url="http://localhost:8000/vehicles/"
+
+    useEffect(() => {
+        fetch(url)
+        .then(response => response.json())
+        .then(data => setVehicles(data))
+    },[]);
 
   return (
     <section style={main_style}>
-        {url}
-        {url.map(vehicle=> (
-            <div>
-                <span>{vehicle.name}</span>
+            {vehicles.map(vehicle=> (
+            <div className='product-card' key={vehicle.id}>
+                <img src={carImage} alt="Product Image" />
+                <div class="product-details">
+                    <div class="product-title">{vehicle.name}</div>
+                    <div class="product-description">{vehicle.brand}</div>
+                    <div class="product-price">{vehicle.year}</div>
+                </div>
             </div>
         )) }
 
